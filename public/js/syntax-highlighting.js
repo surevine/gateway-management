f88d3f64d0cb3e640f80616ef9cdd3983f4835cb
Jonny Heavey <jonny.heavey@surevine.com>
2014-10-29 14:22:26 +0000

    Added basic syntax highlighting to view code screens

---
+++ public/js/syntax-highlighting.js
@@ -0,0 +1,29 @@
+/**
+ * Wrapper around ace editor to provide syntax-highlighting
+ */
+var syntaxHighlighter = {
+
+	/**
+	 * Converts a DOM element to a syntax-highlighted code viewer (read only)
+	 * @param paneElement DOM element to convert to code viewer
+	 */
+	initViewCodePane: function(paneElement) {
+		if($(paneElement).length) {
+			var editor =  ace.edit(paneElement);
+			editor.getSession().setMode("ace/mode/javascript");
+			editor.setReadOnly(true);
+			editor.setOptions({
+		        maxLines: 30
+		    });
+		}
+	},
+
+	/**
+	 * Converts a DOM element to a syntax-highlighted code editor
+	 * @param editorElement DOM element to convert to code editor
+	 */
+	initEditCodePane: function(editorElement) {
+		// TODO
+	}
+
+}
