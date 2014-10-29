f5f2f23ec66abdca716ed6163aa351031bcf3cc5
Jonny Heavey <jonny.heavey@surevine.com>
2014-10-29 13:17:04 +0000

    Implemented basic unit tests for rule file manager

--- test/com/surevine/gateway/rules/RuleFileManagerTest.java
+++ test/com/surevine/gateway/rules/RuleFileManagerTest.java
@@ -1,8 +1,14 @@
 package com.surevine.gateway.rules;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.fail;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 import models.Destination;
 
@@ -10,40 +16,88 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import com.typesafe.config.ConfigFactory;
+
+import play.Logger;
+
 import destinations.DestinationTest;
 
 public class RuleFileManagerTest extends DestinationTest {
 
-	/**
-	 * Create existing destination in database (for use by delete tests)
-	 */
-	@BeforeClass
-	public static void createExistingTestDestination() {
+	@Test
+	public void testCreateDestinationRuleFileDirectory() {
 		Destination destination = new Destination(TEST_EXISTING_DESTINATION_ID, TEST_EXISTING_DESTINATION_NAME, TEST_EXISTING_DESTINATION_URL);
-		destination.save();
+		RuleFileManager ruleFileManager = RuleFileManager.getInstance();
+
+		ruleFileManager.createDestinationRuleFileDirectory(destination);
+
+		Path destinationDirPath = Paths.get(TEST_DESTINATIONS_DIR + "/" + TEST_EXISTING_DESTINATION_ID);
+		assertThat(Files.exists(destinationDirPath)).isEqualTo(true);
+	}
+
+	@Test
+	public void testCreateDestinationRuleFile() {
+		Destination destination = new Destination(TEST_EXISTING_DESTINATION_ID, TEST_EXISTING_DESTINATION_NAME, TEST_EXISTING_DESTINATION_URL);
+
+		RuleFileManager ruleFileManager = RuleFileManager.getInstance();
+		ruleFileManager.createDestinationRuleFile(destination, Destination.DEFAULT_EXPORT_RULEFILE_NAME);
+
+		Path destinationRuleFilePath = Paths.get(TEST_DESTINATIONS_DIR + "/" + TEST_EXISTING_DESTINATION_ID, Destination.DEFAULT_EXPORT_RULEFILE_NAME);
+		assertThat(Files.exists(destinationRuleFilePath)).isEqualTo(true);
+
+		Path templateRuleFilePath = Paths.get(ConfigFactory.load().getString("gateway.template.rule.file"));
+		try {
+			String parsedTemplate = loadFileContents(templateRuleFilePath);
+			String parsedDestinationRuleFile = loadFileContents(destinationRuleFilePath);
+			assertThat(parsedTemplate).isEqualTo(parsedDestinationRuleFile);
+		}
+		catch(IOException e) {
+			fail("Error loading file contents: " + e.getMessage());
+		}
+	}
+
+	@Test
+	public void testDeleteDestinationRuleFileDirectory() {
+		Destination destination = new Destination(TEST_EXISTING_DESTINATION_ID, TEST_EXISTING_DESTINATION_NAME, TEST_EXISTING_DESTINATION_URL);
+
+		RuleFileManager ruleFileManager = RuleFileManager.getInstance();
+		ruleFileManager.deleteDestinationRuleFileDirectory(destination);
+
+		Path destinationDirPath = Paths.get(TEST_DESTINATIONS_DIR + "/" + TEST_EXISTING_DESTINATION_ID);
+		assertThat(Files.exists(destinationDirPath)).isEqualTo(false);
 	}
 
 	@Test
 	@Ignore
-	public static void testCreateDestinationRuleFile() {
+	public void testLoadGlobalExportRules() {
 		// TODO
-		//Boolean exists = Files.exists(rule_file_path);
-		//assertThat(exists).isEqualTo(true);
 	}
 
 	@Test
 	@Ignore
-	public static void testCreateDestinationRuleFileDirectory() {
+	public void testLoadGlobalImportRules() {
 		// TODO
-		//assertThat(Files.exists(destinationDirPath)).isEqualTo(true);
 	}
 
 	@Test
 	@Ignore
-	public static void testDeleteDestinationRuleFileDirectory() {
+	public void testLoadDestinationExportRules() {
 		// TODO
-		//Boolean exists = Files.exists(destinationDirPath);
-		//assertThat(exists).isEqualTo(false);
+	}
+
+	/**
+	 * Helper method to read file contents
+	 * @param filePath path of file to read
+	 * @return String of file contents
+	 * @throws IOException
+	 */
+	private String loadFileContents(Path filePath) throws IOException {
+		List<String> fileLines = Files.readAllLines(filePath, Charset.forName("UTF-8"));
+    	StringBuffer parsedFile = new StringBuffer();
+		for (String line : fileLines) {
+			parsedFile.append(line + System.lineSeparator());
+		}
+		return parsedFile.toString();
 	}
 
 }
