6142988e30395720a3e3440edcd588db5521863c
Jonny Heavey <jonny.heavey@surevine.com>
2014-11-07 14:45:41 +0000

    Added test stubs for remote authenticator

---
+++ test/controllers/WildflyRemoteAuthenticatorTest.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class WildflyRemoteAuthenticatorTest {
+
+	@Test
+	@Ignore
+	public void testGetUsernameExistingSession() {
+		// TODO expect session string
+	}
+
+	@Test
+	@Ignore
+	public void testGetUsernameNewSession() {
+		// TODO expect string from proxy mockup
+	}
+
+	@Test
+	@Ignore
+	public void testGetUsernameUnauthenticatedUser() {
+		// TODO expect null
+	}
+
+	@Test
+	@Ignore
+	public void testGetUsernameServiceDown() {
+		// TODO expect null
+	}
+
+}
