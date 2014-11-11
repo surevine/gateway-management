0f46e2eefaf9dd05f2bd517e9cefed5957927ad1
Jonny Heavey <jonny.heavey@surevine.com>
2014-11-11 13:25:54 +0000

    remove auth restriction on dashboard action

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,7 +4,7 @@ import play.mvc.*;
 
 public class Application extends Controller {
 
-	@Security.Authenticated(WildflyRemoteAuthenticator.class)
+	//@Security.Authenticated(WildflyRemoteAuthenticator.class)
     public static Result index() {
         return ok(views.html.dashboard.index.render("Welcome to the gateway management console."));
     }
