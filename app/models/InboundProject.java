d833c4250e970a3524c7f3faac9c0bdfae640b51
Administrator <user@example.com>
2015-01-24 21:32:29 +0000

    Added form validation for whitelist projects. Fixes #43

--- app/models/InboundProject.java
+++ app/models/InboundProject.java
@@ -1,8 +1,16 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import com.avaje.ebean.Expr;
+
+import play.data.validation.ValidationError;
 import play.data.validation.Constraints.MaxLength;
 import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
@@ -49,5 +57,37 @@ public class InboundProject extends Model {
      */
     public static Model.Finder<Long,InboundProject> find = new Model.Finder<Long,InboundProject>(Long.class, InboundProject.class);
 
+    /**
+     * Perform additional validation (beyond annotations) on model properties.
+     *
+     * @return List<ValidationError> validation error message
+     */
+    public List<ValidationError> validate() {
+
+    	List<ValidationError> errors = new ArrayList<ValidationError>();
+
+    	InboundProject existingProject = find.where()
+    									.ieq("sourceOrganisation", sourceOrganisation)
+    									.ieq("projectKey", projectKey)
+    									.ieq("repositorySlug", repositorySlug)
+    									.not(Expr.eq("id", id))
+    									.findUnique();
+
+    	if(existingProject != null) {
+    		errors.add(new ValidationError("", "Inbound project already exists."));
+    	}
+
+    	Pattern p = Pattern.compile("([^-_a-zA-Z0-9])");
+    	Matcher projectKeyMatcher = p.matcher(projectKey);
+        if (projectKeyMatcher.find()) {
+        	errors.add(new ValidationError("projectKey", "Project key contains invalid characters."));
+        }
+    	Matcher repoSlugMatcher = p.matcher(repositorySlug);
+        if (repoSlugMatcher.find()) {
+        	errors.add(new ValidationError("repositorySlug", "Repository slug contains invalid characters."));
+        }
+
+    	return errors.isEmpty() ? null : errors;
+    }
 
 }
