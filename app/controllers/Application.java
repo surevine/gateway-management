7d472a26a881b5eeeddf64821f4de1391a0ded55
JonnyHeavey <jonnyheavey@gmail.com>
2015-02-23 17:20:35 +0000

    added basic shortcuts to dashboard page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,12 +1,18 @@
 package controllers;
 
+import models.Destination;
+import models.Repository;
 import play.mvc.*;
 
 public class Application extends AuditedController {
 
 	@Security.Authenticated(AppAuthenticator.class)
     public Result index() {
-        return ok(views.html.dashboard.index.render("Welcome to the gateway management console."));
+
+		int destinationCount = Destination.FIND.all().size();
+		int repositoryCount = Repository.FIND.all().size();
+
+        return ok(views.html.dashboard.index.render("Welcome to the gateway management console.", destinationCount, repositoryCount));
     }
 
 }
